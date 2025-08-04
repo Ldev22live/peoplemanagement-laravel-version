746e054f0612e46f6f4808f688b3e61b1529eadc
Lorean <ldev22@live.com>
2025-08-04 22:17:36 +0200

    remove old tests directory as it has it's own repo

--- test/system-includes/navigation.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar navbar-expand-lg bg-light navbar-light py-2 fixed-top">
-    <div class="container">
-        <div class="d-flex align-items-center">
-                            <a href="/test/index.php" class="navbar-brand mb-0">
-                                <span class="h4 fw-bold text-primary">People Management System</span>
-                            </a>
-                    </div>
-    </div>
-</nav>
